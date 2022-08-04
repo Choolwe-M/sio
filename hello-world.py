5409f18d516c85be67e0ef2cbb0af3443de5c733
Choolwe-M <choolwemmweemba@gmail.com>
2022-08-04 14:16:44 +0200

    choolwe mweemba

--- hello-world.py
+++ hello-world.py
@@ -2,3 +2,5 @@ print("My name is Vikwato Kamanga.\n"
       "I am currently pursuing a Masters of Science Degree in Analytics from Georgia Insititute of Technology\n"
       "I enjoy programming, data management and system administration\n"
       "Outside work, I enjoy watching and playing sport, listening to music, playing video games and watching TV.")
+
+print("My name is choolwe mweemba , i am currently a student at the university of inormation and communication pursuing a masters in project management. i completed my degree in 2021 from the copperbelt university")
